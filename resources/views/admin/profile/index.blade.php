3c19360227b031d1c8ec9a022b0f2215961d094d
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-07-30 19:49:28 +0600

    Admin Panel- Admin profile update (part - 1)
    Admin Panel- Admin profile update (part - 1)

---
+++ resources/views/admin/profile/index.blade.php
@@ -0,0 +1,65 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <!-- Main Content -->
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Profile</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item">Profile</div>
+                </div>
+            </div>
+            <div class="section-body">
+                <h2 class="section-title">Hi, {{ Auth::user()->name }}</h2>
+                <p class="section-lead">
+                    Change information about yourself on this page.
+                </p>
+
+                <div class="row mt-sm-4">
+                    <div class="col-12 col-md-12 col-lg-5">
+                        <div class="card profile-widget">
+                            <div class="profile-widget-header">
+                                <img alt="image" src="{{asset('backend/assets/img/avatar/avatar-1.png')}}" class="rounded-circle profile-widget-picture">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-12 col-lg-7">
+                        <div class="card">
+                            <form method="post"  class="needs-validation" novalidate="" action="{{route('admin.profile.update')}}">
+                                @csrf
+                                <div class="card-header">
+                                    <h4>Edit Profile</h4>
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-7 col-12">
+                                            <label>Name</label>
+                                            <input type="text"  name='name' class="form-control" value="{{ Auth::user()->name }}" required="">
+                                            <div class="invalid-feedback">
+                                                Please fill in the first name
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-md-7 col-12">
+                                            <label>Email</label>
+                                            <input type="text" name="email" class="form-control" value="{{ Auth::user()->email }}" required="">
+                                            <div class="invalid-feedback">
+                                                Please fill in the email
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer text-right">
+                                    <button class="btn btn-primary">Save Changes</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,12 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 
 
 /** Admin Routes */
 Route::get('dashboard', [AdminController::class, 'dashboard'])->middleware(['auth', 'role:admin'])->name('admin.dashboard');
+
+/** Profile Routes*/
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('admin.profile.update');
