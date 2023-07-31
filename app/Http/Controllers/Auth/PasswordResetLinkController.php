34266be9ca9b83bf74b77f9a2292da3bad0d7a20
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-07-31 11:46:37 +0600

    Frontend Setup- Master forget password page
    Frontend Setup- Master forget password page

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -36,6 +36,8 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        // Set a success toast, with a title
+        toastr()->success('Reset link has been sent your email successfully!');
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,18 +36,18 @@
                         <ul class="nav nav-pills mb-3" id="pills-tab2" role="tablist">
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link active" id="pills-home-tab2" data-bs-toggle="pill"
-                                        data-bs-target="#pills-homes" type="button" role="tab" aria-controls="pills-homes"
-                                        aria-selected="true">login</button>
+                                    data-bs-target="#pills-homes" type="button" role="tab" aria-controls="pills-homes"
+                                    aria-selected="true">login</button>
                             </li>
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link" id="pills-profile-tab2" data-bs-toggle="pill"
-                                        data-bs-target="#pills-profiles" type="button" role="tab"
-                                        aria-controls="pills-profiles" aria-selected="true">signup</button>
+                                    data-bs-target="#pills-profiles" type="button" role="tab"
+                                    aria-controls="pills-profiles" aria-selected="true">signup</button>
                             </li>
                         </ul>
                         <div class="tab-content" id="pills-tabContent2">
                             <div class="tab-pane fade show active" id="pills-homes" role="tabpanel"
-                                 aria-labelledby="pills-home-tab2">
+                                aria-labelledby="pills-home-tab2">
                                 <div class="wsus__login">
                                     <form method="POST" action="{{ route('login') }}">
                                         @csrf
@@ -65,7 +65,7 @@
                                         <div class="wsus__login_save">
                                             <div class="form-check form-switch">
                                                 <input id="remember_me" name="remember" class="form-check-input" type="checkbox"
-                                                       id="flexSwitchCheckDefault">
+                                                    id="flexSwitchCheckDefault">
                                                 <label class="form-check-label" for="flexSwitchCheckDefault">Remember
                                                     me</label>
                                             </div>
@@ -73,7 +73,7 @@
                                         </div>
 
                                         <button class="common_btn" type="submit">login</button>
-                                 <p class="social_text">Sign in with social account</p>
+                                       <p class="social_text">Sign in with social account</p>
                                         <ul class="wsus__login_link">
                                             <li><a href="#"><i class="fab fa-google"></i></a></li>
                                             <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
@@ -84,7 +84,7 @@
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="pills-profiles" role="tabpanel"
-                                 aria-labelledby="pills-profile-tab2">
+                                aria-labelledby="pills-profile-tab2">
                                 <div class="wsus__login">
                                     <form method="POST" action="{{ route('register') }}">
                                         @csrf
