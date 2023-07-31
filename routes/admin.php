85ecc33234f4d86fc0d07cad4d12c6886af07d41
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-07-31 16:45:29 +0600

    Slider- Backend slider page design
    Slider- Backend slider page design
    
    Route::controller(ItemController::class)->group(function(){
    
        Route::get('items', 'index')->name('items.index');
    
        Route::post('items', 'store')->name('items.store');
    
        Route::get('items/create', 'create')->name('items.create');
    
        Route::get('items/{item}', 'show')->name('items.show');
    
        Route::put('items/{item}', 'update')->name('items.update');
    
        Route::delete('items/{item}', 'destroy')->name('items.destroy');
    
        Route::get('items/{item}/edit', 'edit')->name('items.edit');
    
    });

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 
 
 /** Admin Routes */
@@ -15,3 +16,6 @@ Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
 
+/** Slider Routes*/
+
+Route::resource('slider', SliderController::class);
