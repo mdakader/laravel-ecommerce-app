725e68bd1517e2b4950ae137a64b244ac4d88cde
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-01 15:48:01 +0600

    Child Category- working with edit and delete feature
    Child Category- working with edit and delete feature

--- app/Http/Controllers/Backend/ChildCategoryController.php
+++ app/Http/Controllers/Backend/ChildCategoryController.php
@@ -77,7 +77,11 @@ class ChildCategoryController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $categories = Category::all();
+        $childCategory = ChildCategory::findOrFail($id);
+        $subCategories = SubCategory::where('category_id', $childCategory->category_id)->get();
+
+        return view('admin.child-category.edit', compact('categories', 'childCategory', 'subCategories'));
     }
 
     /**
@@ -85,7 +89,25 @@ class ChildCategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'category' => ['required'],
+            'sub_category' => ['required'],
+            'name' => ['required', 'max:200', 'unique:child_categories,name,'.$id],
+            'status' => ['required']
+        ]);
+
+        $childCategory = ChildCategory::findOrFail($id);
+
+        $childCategory->category_id = $request->category;
+        $childCategory->sub_category_id = $request->sub_category;
+        $childCategory->name = $request->name;
+        $childCategory->slug = Str::slug($request->name);
+        $childCategory->status = $request->status;
+        $childCategory->save();
+
+        toastr('Update Successfully!', 'success');
+
+        return redirect()->route('admin.child-category.index');
     }
 
     /**
@@ -93,6 +115,9 @@ class ChildCategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $childCategory = ChildCategory::findOrFail($id);
+        $childCategory->delete();
+
+        return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
     }
 }
