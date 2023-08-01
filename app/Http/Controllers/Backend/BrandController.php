ba10e74573497e1a102226afe1075ccd908e47b9
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-01 18:00:17 +0600

    Brand- working with update feature
    Brand- working with update feature

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -66,7 +66,8 @@ class BrandController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $brand = Brand::findOrFail($id);
+        return view('admin.brand.edit', compact('brand'));
     }
 
     /**
@@ -74,7 +75,26 @@ class BrandController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'logo' => ['image', 'max:2000'],
+            'name' => ['required', 'max:200'],
+            'is_featured' => ['required'],
+            'status' => ['required']
+        ]);
+
+        $brand = Brand::findOrFail($id);
+
+        $logoPath = $this->updateImage($request, 'logo', 'uploads', $brand->logo);
+
+        $brand->logo = empty(!$logoPath) ? $logoPath : $brand->logo;
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        $brand->is_featured = $request->is_featured;
+        $brand->status = $request->status;
+        $brand->save();
+
+        toastr('Updated Successfully!', 'success');
+        return redirect()->route('admin.brand.index');
     }
 
     /**
--- resources/views/admin/brand/edit.blade.php
+++ resources/views/admin/brand/edit.blade.php
@@ -4,50 +4,54 @@
     <!-- Main Content -->
     <section class="section">
         <div class="section-header">
-            <h1>Category</h1>
+            <h1>Brand</h1>
         </div>
-
         <div class="section-body">
 
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Edit Category</h4>
-
+                            <h4>Edit Brand</h4>
                         </div>
                         <div class="card-body">
-                            <form action="{{route('admin.category.update', $category->id)}}" method="POST">
+                            <form action="{{route('admin.brand.update', $brand->id)}}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 @method('PUT')
                                 <div class="form-group">
-                                    <label>Icon</label>
-                                    <div>
-                                        <button class="btn btn-primary" data-icon="{{$category->icon}}" data-selected-class="btn-danger"
-                                                data-unselected-class="btn-info" role="iconpicker" name="icon" ></button>
-                                    </div>
-
+                                    <label>Preview</label>
+                                    <br>
+                                    <img width="200" src="{{asset($brand->logo)}}" alt="">
                                 </div>
                                 <div class="form-group">
+                                    <label>Logo</label>
+                                    <input type="file" class="form-control" name="logo">
+                                </div>
+
+                                <div class="form-group">
                                     <label>Name</label>
-                                    <input type="text" class="form-control" name="name" value="{{$category->name}}">
+                                    <input type="text" class="form-control" name="name" value="{{$brand->name}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputState">Is Featured</label>
+                                    <select id="inputState" class="form-control" name="is_featured">
+                                        <option {{$brand->is_featured == 1 ? 'selected': ''}} value="1">Yes</option>
+                                        <option {{$brand->is_featured == 0 ? 'selected': ''}} value="0">No</option>
+                                    </select>
                                 </div>
                                 <div class="form-group">
                                     <label for="inputState">Status</label>
                                     <select id="inputState" class="form-control" name="status">
-                                        <option {{$category->status == 1 ? 'selected': ''}} value="1">Active</option>
-                                        <option {{$category->status == 0 ? 'selected': ''}} value="0">Inactive</option>
+                                        <option {{$brand->status == 1 ? 'selected': ''}} value="1">Active</option>
+                                        <option {{$brand->status == 0 ? 'selected': ''}} value="0">Inactive</option>
                                     </select>
                                 </div>
                                 <button type="submmit" class="btn btn-primary">Update</button>
                             </form>
                         </div>
-
                     </div>
                 </div>
             </div>
-
         </div>
     </section>
-
 @endsection
