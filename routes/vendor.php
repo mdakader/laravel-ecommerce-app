b092b4ab04cbebb03553bae0947cb890188b4ffe
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-02 14:13:04 +0600

    Product- Some modification on form and db columns
    Product- Some modification on form and db columns

--- routes/vendor.php
+++ routes/vendor.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Backend\VendorProfileController;
 
 
+
 /** Vendor Routes */
-Route::get('dashboard', [VendorController::class, 'dashboard'])->middleware(['auth', 'role:vendor'])->name('dashboard');
-Route::get('profile', [VendorProfileController::class, 'index'])->middleware(['auth', 'role:vendor'])->name('profile');
-Route::put('profile', [VendorProfileController::class, 'updateProfile'])->middleware(['auth', 'role:vendor'])->name('profile.update'); // user.profile.update
-Route::post('profile', [VendorProfileController::class, 'updatePassword'])->middleware(['auth', 'role:vendor'])->name('profile.update.password');
+Route::get('dashboard', [VendorController::class, 'dashboard'])->name('dashbaord');
+Route::get('profile', [VendorProfileController::class, 'index'])->name('profile');
+Route::put('profile', [VendorProfileController::class, 'updateProfile'])->name('profile.update'); // vendor.profile.update
+Route::post('profile', [VendorProfileController::class, 'updatePassword'])->name('profile.update.password'); // vendor.profile.update.password
