66668861fbe57f2f976972f9ceb55dfe1c432fe9
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-02 12:42:37 +0600

    Product- Creating necessary files and DB columns
     Product- Creating necessary files and DB columns

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -35,6 +35,15 @@
                 </ul>
             </li>
 
+            <li class="dropdown {{setActive(['admin.brand.*'])}}">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Manage Product</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{setActive(['admin.brand.*'])}}"><a class="nav-link" href="{{route('admin.brand.index')}}">Brand</a></li>
+                    <li class="{{setActive(['admin.brand.*'])}}"><a class="nav-link" href="{{route('admin.brand.index')}}">Products</a></li>
+                </ul>
+            </li>
+
+
             <li class="dropdown {{setActive(['admin.vendor-profile.*'])}}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Ecommerce</span></a>
                 <ul class="dropdown-menu">
@@ -42,12 +51,6 @@
                 </ul>
             </li>
 
-            <li class="dropdown {{setActive(['admin.brand.*'])}}">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Manage Product</span></a>
-                <ul class="dropdown-menu">
-                    <li class="{{setActive(['admin.brand.*'])}}"><a class="nav-link" href="{{route('admin.brand.index')}}">Brand</a></li>
-                </ul>
-            </li>
         </ul>
 
         <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
