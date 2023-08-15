f2207a31bf06703e6caeefed7c12448759b1f4ed
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-16 00:11:22 +0600

    Flash Sale- Creating necessary files
    Flash Sale- Creating necessary files

---
+++ app/Http/Controllers/Backend/FlashSaleController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\DataTables\FlashSaleItemDataTable;
+use App\Http\Controllers\Controller;
+use App\Models\FlashSale;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class FlashSaleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(FlashSaleItemDataTable $dataTable)
+    {
+        $flashSaleDate = FlashSale::first();
+        $products = Product::where('is_approved', 1)->where('status', 1)->orderBy('id', 'DESC')->get();
+        return $dataTable->render('admin.flash-sale.index', compact('flashSaleDate', 'products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
