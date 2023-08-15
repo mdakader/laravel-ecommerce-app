005e474b5c715822bfcfd7b6627da6b7cb9d7cb8
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-15 17:27:31 +0600

    Product- working with edit form
    Product- working with edit form

--- app/Http/Controllers/Backend/VendorProductController.php
+++ app/Http/Controllers/Backend/VendorProductController.php
@@ -101,7 +101,26 @@ class VendorProductController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+
+        /** Check if it's the owner of the product */
+        if($product->vendor_id != Auth::user()->vendor->id){
+            abort(404);
+        }
+
+        $subCategories = SubCategory::where('category_id', $product->category_id)->get();
+        $childCategories = ChildCategory::where('sub_category_id', $product->sub_category_id)->get();
+        $categories = Category::all();
+        $brands = Brand::all();
+
+        return view('vendor.product.edit',
+            compact(
+                'product',
+                'subCategories',
+                'childCategories',
+                'categories',
+                'brands'
+            ));
     }
 
     /**
@@ -109,7 +128,58 @@ class VendorProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+
+        $request->validate([
+            'image' => ['nullable', 'image', 'max:3000'],
+            'name' => ['required', 'max:200'],
+            'category' => ['required'],
+            'brand' => ['required'],
+            'price' => ['required'],
+            'qty' => ['required'],
+            'short_description' => ['required', 'max: 600'],
+            'long_description' => ['required'],
+            'seo_title' => ['nullable','max:200'],
+            'seo_description' => ['nullable','max:250'],
+            'status' => ['required']
+        ]);
+
+        $product = Product::findOrFail($id);
+
+        if($product->vendor_id != Auth::user()->vendor->id){
+            abort(404);
+        }
+
+        /** Handle the image upload */
+        $imagePath = $this->updateImage($request, 'image', 'uploads', $product->thumb_image);
+
+        $product->thumb_image = empty(!$imagePath) ? $imagePath : $product->thumb_image;
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->vendor_id = Auth::user()->vendor->id;
+        $product->category_id = $request->category;
+        $product->sub_category_id = $request->sub_category;
+        $product->child_category_id = $request->child_category;
+        $product->brand_id = $request->brand;
+        $product->qty = $request->qty;
+        $product->short_description = $request->short_description;
+        $product->long_description = $request->long_description;
+        $product->video_link = $request->video_link;
+        $product->sku = $request->sku;
+        $product->price = $request->price;
+        $product->offer_price = $request->offer_price;
+        $product->offer_start_date = $request->offer_start_date;
+        $product->offer_end_date = $request->offer_end_date;
+        $product->product_type = $request->product_type;
+        $product->status = $request->status;
+        $product->is_approved = $product->is_approved;
+        $product->seo_title = $request->seo_title;
+        $product->seo_description = $request->seo_description;
+        $product->save();
+
+        toastr('Updated Successfully!', 'success');
+
+        return redirect()->route('vendor.products.index');
+
     }
 
     /**
@@ -120,6 +190,15 @@ class VendorProductController extends Controller
         //
     }
 
+
+    public function changeStatus(Request $request)
+    {
+        $product = Product::findOrFail($request->id);
+        $product->status = $request->status == 'true' ? 1 : 0;
+        $product->save();
+
+        return response(['message' => 'Status has been updated!']);
+    }
 
     /**
      * Get all product sub categores
