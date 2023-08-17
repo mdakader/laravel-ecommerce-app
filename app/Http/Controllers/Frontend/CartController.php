428680d6a09b68ed575ba689478a1e460484a62e
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-17 12:51:38 +0600

    Add To Cart - Working with add to cart feature (part - 2)
    Add To Cart - Working with add to cart feature (part - 2)

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -3,11 +3,54 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductVariantItem;
 use Illuminate\Http\Request;
+use Cart;
 
 class CartController extends Controller
 {
-    public function addToCart(Request $request){
-        dd($request->all());
+    /** Add to cart */
+    public function addToCart(Request $request)
+    {
+
+        $product = Product::findOrFail($request->product_id);
+        $variants = [];
+
+        $variantTotalAmount = 0;
+        if ($request->has('variants_items')) {
+            foreach ($request->variants_items as $item_id) {
+                $variantItem = ProductVariantItem::find($item_id);
+                $variants[$variantItem->productVariant->name]['name'] = $variantItem->name;
+                $variants[$variantItem->productVariant->price]['price'] = $variantItem->price;
+                $variantTotalAmount += $variantItem->price;
+            }
+        }
+
+
+        /**  Check discount  */
+
+        $productPrice = 0;
+
+        if (checkDiscount($product)) {
+            $productPrice = ($product->offer_price + $variantTotalAmount);
+        } else {
+            $productPrice = ($product->price + $variantTotalAmount);
+        }
+
+        $cartData = [];
+        $cartData['id'] = $product->id;
+        $cartData['name'] = $product->name;
+        $cartData['qty'] = $product->qty;
+        $cartData['price'] = $productPrice;
+        $cartData['weight'] = 10;
+        $cartData['options']['variants'] = $variants;
+        $cartData['options']['image'] = $product->thumb_image;
+        $cartData['options']['slug'] = $product->slug;
+
+        Cart::add($cartData);
+
+        return response(['status' => 'success', 'message' => 'Added to cart successfully!']);
+
     }
 }
