fb393a6b4f456a76f9b60871eae6c2505b7dc69a
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-18 16:34:37 +0600

    Handle Order - delete order
    Handle Order - delete order

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -106,7 +106,16 @@ class OrderController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $order = Order::findOrFail($id);
+
+        // delete order products
+        $order->orderProducts()->delete();
+        // delete transaction
+        $order->transaction()->delete();
+
+        $order->delete();
+
+        return response(['status' => 'success', 'message' => 'Deleted successfully!']);
     }
 
 
