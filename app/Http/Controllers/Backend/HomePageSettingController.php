7393d638afe3f1557f16510de90cfe892f1ab869
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-19 13:32:21 +0600

    Backend- working with single category product slider
    Backend- working with single category product slider

--- app/Http/Controllers/Backend/HomePageSettingController.php
+++ app/Http/Controllers/Backend/HomePageSettingController.php
@@ -13,13 +13,31 @@ class HomePageSettingController extends Controller
     {
         $categories = Category::where('status', 1)->get();
         $popularCategorySection = HomePageSetting::where('key', 'popular_category_section')->first();
-        return view('admin.home-page-setting.index', compact('categories', 'popularCategorySection'));
+        $sliderSectionOne = HomePageSetting::where('key', 'product_slider_section_one')->first();
+        return view('admin.home-page-setting.index', compact(
+            'categories',
+            'popularCategorySection',
+            'sliderSectionOne'
+        ));
     }
 
 
     public function updatePopularCategorySection(Request $request)
     {
+        $request->validate([
+            'cat_one' => ['required'],
+            'cat_two' => ['required'],
+            'cat_three' => ['required'],
+            'cat_four' => ['required']
 
+        ], [
+            'cat_one.required' => 'Category one filed is required',
+            'cat_two.required' => 'Category two filed is required',
+            'cat_three.required' => 'Category three filed is required',
+            'cat_four.required' => 'Category four filed is required',
+        ]);
+
+        // dd($request->all());
         $data = [
             [
                 'category' => $request->cat_one,
@@ -42,7 +60,7 @@ class HomePageSettingController extends Controller
                 'child_category' => $request->child_cat_four,
             ]
         ];
-//        dd($request->all());
+
         HomePageSetting::updateOrCreate(
             [
                 'key' => 'popular_category_section'
@@ -55,5 +73,34 @@ class HomePageSettingController extends Controller
         toastr('Updated successfully!', 'success', 'success');
 
         return redirect()->back();
+    }
+
+    public function updateProductSliderSectionOn(Request $request)
+    {
+        $request->validate([
+            'cat_one' => ['required']
+        ], [
+            'cat_one.required' => 'Category filed is required'
+        ]);
+
+        $data = [
+            'category' => $request->cat_one,
+            'sub_category' => $request->sub_cat_one,
+            'child_category' => $request->child_cat_one,
+        ];
+
+        HomePageSetting::updateOrCreate(
+            [
+                'key' => 'product_slider_section_one'
+            ],
+            [
+                'value' => json_encode($data)
+            ]
+        );
+
+        toastr('Updated successfully!', 'success', 'success');
+
+        return redirect()->back();
+
     }
 }
