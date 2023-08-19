16c2d250b3693f94b953ef3d9b7e42dedab44152
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-19 13:41:35 +0600

    Frontend - Show category slider one products at frontend
    Frontend - Show category slider one products at frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -20,13 +20,15 @@ class HomeController extends Controller
         $popularCategory = HomePageSetting::where('key', 'popular_category_section')->first();
         $brands = Brand::where('status', 1)->where('is_featured', 1)->get();
         $typeBaseProducts = $this->getTypeBaseProduct();
+        $categoryProductSliderSectionOne = HomePageSetting::where('key', 'product_slider_section_one')->first();
         return view('frontend.home.home', compact(
             'sliders',
             'flashSaleDate',
             'flashSaleItems',
             'popularCategory',
             'brands',
-            'typeBaseProducts'
+            'typeBaseProducts',
+            'categoryProductSliderSectionOne'
         ));
     }
 
