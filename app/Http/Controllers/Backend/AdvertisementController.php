d07d708b525a65344637d0563b49608c9efe0921
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-20 12:28:54 +0600

    Ad Feature - Creating form fields and handle them (part-2)
    Ad Feature - Creating form fields and handle them (part-2)

--- app/Http/Controllers/Backend/AdvertisementController.php
+++ app/Http/Controllers/Backend/AdvertisementController.php
@@ -16,8 +16,12 @@ class AdvertisementController extends Controller
         $homepage_secion_banner_one = Adverisement::where('key', 'homepage_secion_banner_one')->first();
         $homepage_secion_banner_one = json_decode($homepage_secion_banner_one->value);
 
+        $homepage_secion_banner_two = Adverisement::where('key', 'homepage_secion_banner_two')->first();
+        $homepage_secion_banner_two = json_decode($homepage_secion_banner_two?->value);
+
         return view('admin.advertisement.index', compact(
             'homepage_secion_banner_one',
+            'homepage_secion_banner_two'
         ));
     }
 
@@ -59,6 +63,50 @@ class AdvertisementController extends Controller
     public function homepageBannerSecionTwo(Request $request)
     {
 
+        $request->validate([
+            'banner_one_image' => ['image'],
+            'banner_one_url' => ['required'],
+            'banner_two_image' => ['image'],
+            'banner_two_url' => ['required']
+        ]);
+
+        /** Handle the image upload */
+        $imagePath = $this->updateImage($request, 'banner_one_image', 'uploads');
+        $imagePathTwo = $this->updateImage($request, 'banner_two_image', 'uploads');
+
+
+        $value = [
+            'banner_one' => [
+                'banner_url' => $request->banner_one_url,
+                'status' => $request->banner_one_status == 'on' ? 1 : 0
+            ],
+            'banner_two' => [
+                'banner_url' => $request->banner_two_url,
+                'status' => $request->banner_two_status == 'on' ? 1 : 0
+            ]
+        ];
+        if(!empty($imagePath)){
+            $value['banner_one']['banner_image'] = $imagePath;
+        }else {
+
+            $value['banner_one']['banner_image'] = $request->banner_one_old_image;
+        }
+        if(!empty($imagePathTwo)){
+            $value['banner_two']['banner_image'] = $imagePathTwo;
+        }else {
+
+            $value['banner_two']['banner_image'] = $request->banner_two_old_image;
+        }
+
+        $value = json_encode($value);
+        Adverisement::updateOrCreate(
+            ['key' => 'homepage_secion_banner_two'],
+            ['value' => $value]
+        );
+
+        toastr('Updated Successfully!', 'success', 'success');
+
+        return redirect()->back();
     }
 
     public function homepageBannerSecionThree(Request $request)
