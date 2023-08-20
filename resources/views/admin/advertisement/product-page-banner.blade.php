d31ad6a55e28ca7436df567296f2b1a62d7b261a
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-20 12:38:58 +0600

    Ad Feature - Creating form fields and handle them (part-4)
    Ad Feature - Creating form fields and handle them (part-4)

--- resources/views/admin/advertisement/product-page-banner.blade.php
+++ resources/views/admin/advertisement/product-page-banner.blade.php
@@ -1,1 +1,35 @@
-<?php
+
+<div class="tab-pane fade " id="list-product" role="tabpanel" aria-labelledby="list-product-list">
+    <div class="card border">
+        <div class="card-body">
+            <form action="{{route('admin.productpage-banner')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+
+                <div class="form-group">
+                    <label for="">Status</label>
+                    <br>
+                    <label class="custom-switch mt-2">
+                        <input type="checkbox"name="status" class="custom-switch-input" >
+                        <span class="custom-switch-indicator"></span>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <img src="" alt="" width="150px">
+                </div>
+                <div class="form-group">
+
+                    <label>Banner Image</label>
+                    <input type="file" class="form-control" name="banner_image" value="">
+                    <input type="hidden" class="form-control" name="banner_old_image" value="">
+                </div>
+                <div class="form-group">
+                    <label>Banner url</label>
+                    <input type="text" class="form-control" name="banner_url" value="">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Update</button>
+            </form>
+        </div>
+    </div>
+</div>
