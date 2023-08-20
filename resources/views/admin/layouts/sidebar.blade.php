70fd22a904ad37d8a92bfd8e64afad422c76636e
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-20 16:25:12 +0600

    Review System - show user create reviews at admin dashboard
    Review System - show user create reviews at admin dashboard

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -50,12 +50,15 @@
                         'admin.products-image-gallery.*',
                         'admin.products-variant.*',
                         'admin.products-variant-item.*',
+                        'admin.reviews.*',
                     ]) }}">
                         <a class="nav-link" href="{{ route('admin.products.index') }}">Products</a></li>
                     <li class="{{ setActive(['admin.seller-products.*']) }}"><a class="nav-link"
                                                                                 href="{{ route('admin.seller-products.index') }}">Seller Products</a></li>
                     <li class="{{ setActive(['admin.seller-pending-products.*']) }}"><a class="nav-link"
                                                                                         href="{{ route('admin.seller-pending-products.index') }}">Seller Pending Products</a></li>
+                    <li class="{{ setActive(['admin.reviews.*']) }}"><a class="nav-link"
+                                                                        href="{{ route('admin.reviews.index') }}">Product Reviews</a></li>
                 </ul>
             </li>
             <li
--- resources/views/frontend/dashboard/layouts/sidebar.blade.php
+++ resources/views/frontend/dashboard/layouts/sidebar.blade.php
@@ -14,8 +14,7 @@
             <li><a class="{{setActive(['vendor.dashbaord'])}}" href="{{route('vendor.dashbaord')}}"><i class="fas fa-tachometer"></i>Go to Vendor Dashboard</a></li>
         @endif
         <li><a class="{{setActive(['user.orders.*'])}}" href="{{route('user.orders.index')}}"><i class="fas fa-list-ul"></i> Orders</a></li>
-        <li><a href="dsahboard_download.html"><i class="far fa-cloud-download-alt"></i> Downloads</a></li>
-        <li><a href="dsahboard_review.html"><i class="far fa-star"></i> Reviews</a></li>
+        <li><a class="{{setActive(['user.review.*'])}}" href="{{route('user.review.index')}}"><i class="far fa-star"></i> Reviews</a></li>
         <li><a href="dsahboard_wishlist.html"><i class="far fa-heart"></i> Wishlist</a></li>
         <li><a href="{{route('user.profile')}}"><i class="far fa-user"></i> My Profile</a></li>
         <li><a href="{{route('user.address.index')}}"><i class="fal fa-gift-card"></i> Addresses</a></li>
