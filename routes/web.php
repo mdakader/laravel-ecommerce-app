576ed460ee0672c2b9641620285d0919cdd8944d
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-21 19:23:07 +0600

    Frontend- Working with vendor page
    Frontend- Working with vendor page and vendor product page

--- routes/web.php
+++ routes/web.php
@@ -65,6 +65,9 @@ Route::get('coupon-calculation', [CartController::class, 'couponCalculation'])->
 Route::post('newsletter-request', [NewsletterController::class, 'newsLetterRequset'])->name('newsletter-request');
 Route::get('newsletter-verify/{token}', [NewsletterController::class, 'newsLetterEmailVarify'])->name('newsletter-verify');
 
+/** vendor page routes */
+Route::get('vendor', [HomeController::class, 'vendorPage'])->name('vendor.index');
+Route::get('vendor-product/{id}', [HomeController::class, 'vendorProductsPage'])->name('vendor.products');
 
 Route::group(['middleware' => ['auth', 'verified'], 'prefix'=> 'user', 'as'=>'user.'], function()
 {
