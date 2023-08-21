f7f2cb163e04000eac224e50b78852ef6f405bb5
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-21 22:41:44 +0600

    Frontend- contact page (part - 2)
    Frontend- contact page (part - 2)

--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,10 @@ Route::get('about', [PageController::class, 'about'])->name('about');
 /** terms and conditions page route */
 Route::get('terms-and-conditions', [PageController::class, 'termsAndCondition'])->name('terms-and-conditions');
 
+/** contact route */
+Route::get('contact', [PageController::class, 'contact'])->name('contact');
+Route::post('contact', [PageController::class, 'handleContactForm'])->name('handle-contact-form');
+
 Route::group(['middleware' => ['auth', 'verified'], 'prefix'=> 'user', 'as'=>'user.'], function()
 {
     Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
