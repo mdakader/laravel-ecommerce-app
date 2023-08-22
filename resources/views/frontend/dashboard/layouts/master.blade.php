5f88054a0c6393cebc234563e7ad0a0f24ddb036
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-22 08:30:07 +0600

    Vendor Dashboard - working with user dashboard (part - 1)
    Vendor Dashboard - working with user dashboard (part - 1)

--- resources/views/frontend/dashboard/layouts/master.blade.php
+++ resources/views/frontend/dashboard/layouts/master.blade.php
@@ -40,8 +40,8 @@
 ==============================-->
 <div class="wsus__dashboard_menu">
     <div class="wsusd__dashboard_user">
-        <img src="{{Auth::user()->image ? asset(Auth::user()->image) : asset('frontend/images/dashboard_user.jpg')}}" alt="img" class="img-fluid">
-        <p>{{Auth::user()->name}}</p>
+        <img src="{{asset(auth()->user()->image)}}" alt="img" class="img-fluid">
+        <p>{{auth()->user()->name}}</p>
     </div>
 </div>
 <!--=============================
