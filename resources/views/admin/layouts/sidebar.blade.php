8bd08b73bf03bcc2cbc6354c484ea3b79ac07992
Md Abdul Kader <31106100+mdakader@users.noreply.github.com>
2023-08-22 08:45:42 +0600

    Admin Dashboard - working with admin dashboard (part - 2)
    Admin Dashboard - working with admin dashboard (part - 2)

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="{{route('admin.dashboard')}}">Stisla</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
@@ -11,7 +11,7 @@
             <li class="dropdown active">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
-                    <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+                    <li class=active><a class="nav-link" href="{{route('admin.dashboard')}}">General Dashboard</a></li>
                     <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
                 </ul>
             </li>
